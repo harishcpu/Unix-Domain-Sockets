2f1bf635e79bf1b12a193bf1489bbe837423c12c
harishkumar101 <harishec031@gmail.com>
2021-08-04 05:33:52 +0530

    Server Implementation: To handle multiple clients

--- server.c
+++ server.c
@@ -8,6 +8,58 @@
 #define SOCKET_NAME "/tmp/DemoSocket"
 #define BUFFER_SIZE 128
 
+#define MAX_CLIENT_SUPPORTED 32
+
+int monitored_fd_set[MAX_CLIENT_SUPPORTED];
+
+int client_result[MAX_CLIENT_SUPPORTED];
+
+static void 
+initialize_monitor_fd_set() {
+	for(int i = 0; i < MAX_CLIENT_SUPPORTED; i++)
+		monitored_fd_set[i] = -1;
+}
+
+static void 
+add_to_monitored_fd_set(int skt_fd) {
+	for(int i = 0; i < MAX_CLIENT_SUPPORTED; i++) {
+		if(monitored_fd_set[i] != -1)
+			continue;
+		monitored_fd_set[i] = skt_fd;
+		break;
+	}
+}
+
+static void remove_from_monitored_fd_set(int skt_fd) {
+	for(int i = 0; i < MAX_CLIENT_SUPPORTED; i++) {
+		if(monitored_fd_set[i] == skt_fd) {
+			monitored_fd_set[i] = -1;
+			break;
+		}
+	}
+}
+
+static void
+refresh_fd_set(fd_set *fd_set_ptr) {
+	FD_ZERO(fd_set_ptr);
+	for(int i = 0; i < MAX_CLIENT_SUPPORTED; i++) {
+		if(monitored_fd_set[i] != -1) {
+			FD_SET(monitored_fd_set[i], fd_set_ptr);
+		}
+	}
+}
+
+static int
+get_max_fd() {
+	int max = -1;
+	for(int i = 0; i < MAX_CLIENT_SUPPORTED; i++) {
+		if(monitored_fd_set[i] > max)
+			max = monitored_fd_set[i];	
+	}
+
+	return max;
+}
+
 int main(int argc, char *argv[]) {
 	
 	struct sockaddr_un name;
@@ -23,6 +75,10 @@ int main(int argc, char *argv[]) {
 	int result;
 	int data;
 	char buffer[BUFFER_SIZE];
+
+	fd_set readfds;
+	int comm_socket_fd, i;
+	initialize_monitor_fd_set();
 
 	/* Remove the socket, if already exists */
 	unlink(SOCKET_NAME);
@@ -60,52 +116,66 @@ int main(int argc, char *argv[]) {
 		exit(EXIT_FAILURE);
 	}
 
+	/* add master socket to monitored set of FDs */
+	add_to_monitored_fd_set(connection_socket);
+
 	while(1) {
+		refresh_fd_set(&readfds);
+
 		/* wait for incoming connection request */
 		printf("waiting for accept() sys call\n");
+		
+		select(get_max_fd() + 1, &readfds, NULL, NULL, NULL);
+		
+		if(FD_ISSET(connection_socket, &readfds)) {
+			printf("New connection received recvd, accept the connection\n");
 
-		data_socket = accept(connection_socket, NULL, NULL);
-		if(data_socket == -1) {
-			perror("accept");
-			exit(EXIT_FAILURE);
-		}
+			data_socket = accept(connection_socket, NULL, NULL);
 
-		printf("Connection accepted from client\n");
-
-		result = 0;
-		while(1) {
-			/* Prepare the buffer to recv the data */
-			memset(buffer, 0, BUFFER_SIZE);
-
-			/* wait for the next data packet to arrive */
-			printf("waiting for data from client");
-			ret = read(data_socket, buffer, BUFFER_SIZE);
-			if(ret == -1) {
-				perror("read");
+			if(data_socket == -1) {
+				perror("accept");
 				exit(EXIT_FAILURE);
 			}
 
-			/* Add received command */
-			memcpy(&data, buffer, sizeof(int));
-			if(data == 0) break;
-			result += data;
-		}
+			printf("Connection accepted from client\n");
 
-		/* send computed result */
-		memset(buffer, 0, BUFFER_SIZE);
-		sprintf(buffer, "Result: %d", result);
+			add_to_monitored_fd_set(data_socket);
+		} else {
+			comm_socket_fd = -1;
+			for(int i = 0; i < MAX_CLIENT_SUPPORTED; i++) {
+				if(FD_ISSET(monitored_fd_set[i], &readfds)) {
+					comm_socket_fd = monitored_fd_set[i];
+					memset(buffer, 0, BUFFER_SIZE);
+					printf("waiting for data from the client\n");
+					ret = read(comm_socket_fd, buffer, BUFFER_SIZE);
 
-		printf("sending final result back to client\n");
-		ret = write(data_socket, buffer, BUFFER_SIZE);
-		if(ret == -1) {
-			perror("write");
-			exit(EXIT_FAILURE);
-		}
+					if(ret == -1) {
+						perror("read");
+						exit(EXIT_FAILURE);
+					}
 
-		/* close data socket */
-		close(data_socket);
-	}
+					memcpy(&data, buffer, sizeof(int));
+					if(data == 0) {
+						memset(buffer, 0, BUFFER_SIZE);
+						sprintf(buffer, "Result = %d", client_result[i]);
+						
+						printf("sending final result back to client\n");
+						ret = write(comm_socket_fd, buffer, BUFFER_SIZE);
+						if(ret == -1) {
+							perror("write");
+							exit(EXIT_FAILURE);
+						}
 
+						close(comm_socket_fd);
+						client_result[i] = 0;
+						remove_from_monitored_fd_set(comm_socket_fd);
+						continue;
+					}
+					client_result[i] += data;
+				}
+			}
+		} /* go to the select() and block */
+	}	
 	/* close the connection socket */
 	close(connection_socket);
 	printf("connection closed. Good Bye!\n");
